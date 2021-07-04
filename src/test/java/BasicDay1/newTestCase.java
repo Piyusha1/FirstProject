0b39583f6db2b4137554c909e2ce967fc8de80ed
Piyusha1 <psjingare@gmail.com>
2021-07-04 16:53:38 +0530

    Added new testcase

---
+++ src/test/java/BasicDay1/newTestCase.java
@@ -0,0 +1,20 @@
+package BasicDay1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class newTestCase {
+	
+	@Test
+	public void newCase1(){
+		System.out.println("New case1");
+		Assert.assertEquals(45, 45);
+	}
+	
+	@Test
+	public void newCase2(){
+		System.out.println("New case2");
+		Assert.assertEquals(45, 42);
+	}
+
+}
